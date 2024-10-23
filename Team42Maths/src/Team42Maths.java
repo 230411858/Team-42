5228d28b2a0ed2f9f0126a18fdc4d659e918456c
Hamim Chowdhury <230411858@aston.ac.uk>
2024-10-23 11:27:30 +0100

    Updated files

---
+++ Team42Maths/src/Team42Maths.java
@@ -0,0 +1,155 @@
+package Team42Maths.src;
+
+public class Team42Maths {
+    
+    public static void main(String[] args)
+    {
+        test();
+    }
+
+    public static class Maths {
+        
+        public int max(int num1, int num2)
+        {
+            // complete implementation
+        }
+
+        public int min(int num1, int num2)
+        {
+            // complete implementation
+        }
+
+        public int add(int num1, int num2)
+        {
+            // complete implementation
+        }
+
+        public int subtract(int num1, int num2)
+        {
+            // complete implementation
+        }
+
+        public int multiply(int num1, int num2)
+        {
+            // complete implementation
+        }
+
+        public int divide(int num1, int num2)
+        {
+            // complete implementation
+        }
+
+        public int power(int num1, int num2)
+        {
+            // complete implementation
+        }
+
+        public int mod(int num1, int num2)
+        {
+            // complete implementation
+        }
+
+        public float floor(float num1, float num2)
+        {
+            // complete implementation
+        }
+
+        public float ceil(float num1, float num2)
+        {
+            // complete implementation
+        }
+    }
+
+    public static void test()
+    {
+        if (max(2, 4) == 4)
+        {
+            System.out.println("Max test passed!");
+        }
+        else
+        {
+            System.out.println("Max test failed!");
+        }
+
+        if (min(8, 4) == 4)
+        {
+            System.out.println("Min test passed!");
+        }
+        else
+        {
+            System.out.println("Min test failed!");
+        }
+
+        if (add(2, 2) == 4)
+        {
+            System.out.println("Addition test passed!");
+        }
+        else
+        {
+            System.out.println("Addition test failed!");
+        }
+
+        if (sub(7, 3) == 4)
+        {
+            System.out.println("Subtraction test passed!");
+        }
+        else
+        {
+            System.out.println("Subtraction test failed!");
+        }
+
+        if (multiply(3, 4) == 12)
+        {
+            System.out.println("Multiplication test passed!");
+        }
+        else
+        {
+            System.out.println("Multiplication test failed!");
+        }
+
+        if (divide(8, 4) == 2)
+        {
+            System.out.println("Division test passed!");
+        }
+        else
+        {
+            System.out.println("Division test failed!");
+        }
+
+        if (power(2, 3) == 8)
+        {
+            System.out.println("Power test passed!");
+        }
+        else
+        {
+            System.out.println("Power test failed!");
+        }
+
+        if (mod(5, 2) == 1)
+        {
+            System.out.println("Modulo test passed!");
+        }
+        else
+        {
+            System.out.println("Modulo test failed!");
+        }
+
+        if (ceil(4.5) == 5)
+        {
+            System.out.println("Ceiling test passed!");
+        }
+        else
+        {
+            System.out.println("Ceiling test failed!");
+        }
+
+        if (floor(4.5) == 4)
+        {
+            System.out.println("Floor test passed!");
+        }
+        else
+        {
+            System.out.println("Floor test failed!");
+        }
+    }
+}
